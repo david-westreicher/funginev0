21d3f9b5670285a54454df69a88a0e0bf316b501
David Westreicher <david@mohemian.at>
2012-06-09 13:44:44 +0200

    adds jinput and pong game

--- src/io/IO.java
+++ src/io/IO.java
@@ -1,21 +1,28 @@
 package io;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.reflect.Type;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 import settings.Settings;
+import util.Log;
 
 public class IO {
 
 	public static BufferedReader read(String file) {
 		BufferedReader in = null;
+		file = Settings.RESSOURCE_FOLDER + file;
 		try {
-			in = new BufferedReader(new FileReader(Settings.RESSOURCE_FOLDER
-					+ file));
+			in = new BufferedReader(new FileReader(file));
 		} catch (FileNotFoundException e) {
-			e.printStackTrace();
+			Log.err(IO.class, "couldn't find: " + file);
 		}
 		return in;
 	}
@@ -36,4 +43,34 @@ public class IO {
 		return sb.toString();
 	}
 
+	public static <T> T[] readFromJson(String file, Type t) {
+		BufferedReader br = read(file);
+		if (br == null)
+			return null;
+		T result[] = new Gson().fromJson(br, t);
+		try {
+			br.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	public static <T> void writeToJson(String file, T[] players) {
+		String json = new GsonBuilder().setPrettyPrinting().create()
+				.toJson(players);
+		write(file, json);
+	}
+
+	private static void write(String file, String json) {
+		file = Settings.RESSOURCE_FOLDER + file;
+		BufferedWriter out;
+		try {
+			out = new BufferedWriter(new FileWriter(file));
+			out.write(json);
+			out.close();
+		} catch (IOException e) {
+			Log.err(IO.class, "couldn't write to file " + file);
+		}
+	}
 }
